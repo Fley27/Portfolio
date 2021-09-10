7e9651f8719a17f46d3d26fd6a7e628fce35b2b1
Fenley Jude Viky Menelas <fenley@Fenleys-Mini.domain.name>
2021-09-10 18:56:47 -0400

    Morgan dependency has bee add to backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 let port = process.env.PORT || 5000; 
 
 
+app.use(morgan('tiny'));
 app.use('/api/email', require("./routes/email")); 
 
 app.use(express.static('client/build'));
