c0c0446ff33ee9c50aa56c28e6c0979cd45def75
Fenley Jude Viky Menelas <fenley@Fenleys-Mini.domain.name>
2021-09-11 03:53:45 -0400

    last update

--- client/src/component/about.js
+++ client/src/component/about.js
@@ -10,14 +10,10 @@ const About = (props) =>{
                     <div>Me, Myself and I</div>
                 </div>
                 <div className = "work-description">
-                    I am passionate about technology and since my childhood.  I started coding at a very young age (14 years old), so after high school, I naturally switched to computer engineering. <br/>
+                    I am passionate about technology and since my childhood.  I started coding at a very young age (14 years old), so after high school, I naturally switched to computer engineering. <br/><br/>
                     With several years of experience, I master several programming languages such as Java, C, C#, and Javascript. 
                     But the last one that I mentioned above, JavaScript, is my top skill. It has so many powerful and impressive frameworks and libraries,
                     such as React & NodeJs, that allow us to save a lot of time and also create beautiful and powerful tools, like modern and attractive websites, web and mobile native applications. 
-                    <br/>
-                    Each project is an act of commitment to myself and you to meet your needs. However, together we will create the tool that suits best your business or your idea.<br/>
-
-                    I will consider every detail that you share with me, discuss them, and, if necessary, give you some suggestions that may help. I will make sure that the architecture of your project takes into account your immediate and future needs.
                 </div>
                     <div className = "social-media-link-container">
                         <div className = "social-media-link">
