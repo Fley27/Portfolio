fc1cb8ab5970bf2e5cdbe1c49174269065da6670
Fenley Menelas <fleyvick@outlook.com>
2022-03-20 05:32:54 -0400

    OK

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static('client/build'));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(9000);
+let PORT = process.env.PORT || 6000; 
+
+
+app.listen(PORT)
+    .on('listening', () =>
+    console.log(`Realtime server running on port ${PORT}`)
+);
