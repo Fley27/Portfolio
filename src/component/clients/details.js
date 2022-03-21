97a7937ef9a12f910888018371ed4df69f92c4b6
Fenley Menelas <fleyvick@outlook.com>
2022-03-21 02:10:26 -0400

    Project is restructured

---
+++ src/component/clients/details.js
@@ -0,0 +1,64 @@
+import React from "react"
+import "../../styles/details.css"
+
+const Details = ({Image, subtitle, description, description2, description3, description4}) => (
+    <div className = "details">
+        <div className = "activity">
+            <div className = "details-activity-image animated-background">
+                <img
+                    alt = "details"
+                    src = {Image}
+                    loading  = {"lazy"}
+                    className = "details-avatar-image"
+                />
+            </div>
+            <div className = "details-text">
+                <div className = "details-title">
+                    THE DETAILS
+                </div>
+                <div className = "details-subtitle">
+                    {subtitle}
+                </div>
+                <div className = "details-description">
+                    {
+                        description
+                    }
+                <br/><br/>
+                    {
+                        description2
+                    }
+                {
+                    description3 ? (
+                        <>
+                            <br/><br/>
+                            {
+                                description3
+                            }
+                        </>
+                    ):null
+                }
+                {
+                    description4 ? (
+                        <>
+                            <br/><br/>
+                            {
+                                description4
+                            }
+                        </>
+                    ):null
+                }
+                </div>
+                <div className = "buttons">
+                        <button onClick = {()=> window.location.href = "#about"} className = "button-header-contact about">
+                            ABOUT FLEY
+                        </button>
+                        <button onClick = {()=> window.location.href = "/contact"} className = "button-header-contact book">
+                            CONTACT
+                        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Details
---
+++ src/component/home/brand.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "../../images/beautiful-young-curly-brunette-darkskinned-woman-in-colorful-blouse-and-orange-sunglasses-listens-to-music-in-headphones-and-sings-outside-min.jpg"
+import "../../styles/brand.css"
+
+const Brand = () => (
+    <div className = "brand">
+        <div className = "brand-image-container animated-background">
+            <img
+                alt = "Brand"
+                src = {Image}
+                loading  = {"lazy"}
+                className = "brand-image" 
+            />
+        </div>
+        <div className = "brand-text">
+            <div className = "brand-title">
+                BUILDING  YOUR  PERSONAL  BRAND
+            </div>
+            <div className = "brand-description">
+            Building a brand online doesn't have to be that hard. Because you have billions of potential customers who are increasingly 
+            used to spending money online, and just need a little motivation to spend it on you. My ultimate goal is to get these potential 
+            customers to spend or invest their money in your brand. I want them to stay connected to the brand, engage with you on social media and 
+            spend money when you release new content.
+            <br/><br/>
+            As an entrepreneur, I understand the time investment involved in always being on the go. 
+            Let me take care of the website, and at the same time I will take the opportunity to automate 
+            repetitive or recurring actions to save you time and energy while you do what you do best.
+            </div>
+        </div>
+    </div>
+)
+
+export default Brand
